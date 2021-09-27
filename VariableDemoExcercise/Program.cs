5ece17d7cc9bb1e3a4db621cecce317e9dbf2275
bisu07 <g26.bishal@gmail.com>
2021-09-27 10:46:45 -0500

    initial commit

---
+++ VariableDemoExcercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariableDemoExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Ralph";
+            int dogAge = 10;
+            Console.WriteLine($"My dog's name is {dogName},He is {dogAge}years old");
+           
+        }
+    }
+}
